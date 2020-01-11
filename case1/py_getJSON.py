f2bab00a842e5d6c4b74607035b3a4e133196d77
steven880125 <s902196@gmail.com>
2020-01-11 12:42:10 +0800

    Update py_getJSON.py

--- case1/py_getJSON.py
+++ case1/py_getJSON.py
@@ -4,4 +4,6 @@
 import requests
 import json
 data = requests.get(url="http://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=3")
+with open("music.json","w",encoding="utf-8") as myFile:
+    json.dump(data.json(), myFile,ensure_ascii=False)
 myFile.close()
