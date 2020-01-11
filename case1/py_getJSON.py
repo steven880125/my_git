e8d0858358b45f39db0d1d551d74dc39f568995e
steven880125 <s902196@gmail.com>
2020-01-11 12:41:42 +0800

    Create py_getJSON.py

---
+++ case1/py_getJSON.py
@@ -0,0 +1,7 @@
+# Using Python to get JSON from http and save as json file
+# by seaniwei
+
+import requests
+import json
+data = requests.get(url="http://cloud.culture.tw/frontsite/trans/SearchShowAction.do?method=doFindTypeJ&category=3")
+myFile.close()
